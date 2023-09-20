85a1c1554cce273265d12ce7122ecde521fffc69
Pip-1 <philippog@gmail.com>
2023-09-20 21:21:00 +0200

    adding variables

--- daily_weather_etl.py
+++ daily_weather_etl.py
@@ -1,13 +1,16 @@
 import pandas as pd
 from datetime import datetime
+import json
 import requests
 
 access_key = "2d90d10eae259b217aee8119adc6ae70"
 
 #creating variable to pass todays date to the API
 today = datetime.today().strftime("%m/%d")
+city = "Erfurt"
 
 #creating variable for the response from the API
-response = requests.get(f"http://numbersapi.com/{today}/date")
-
 #response.text would output the response from the API
+response_weather = requests.get("http://api.weatherstack.com/current?access_key=2d90d10eae259b217aee8119adc6ae70&query={city}")
+
+
