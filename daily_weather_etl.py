c4cb2f98c3fcae96ec319d9cf01fad2725419020
Pip-1 <philippog@gmail.com>
2023-09-20 21:33:31 +0200

    more trials in notebook

--- daily_weather_etl.py
+++ daily_weather_etl.py
@@ -11,14 +11,14 @@ city = "Erfurt"
 
 #creating variable for the response from the API
 #response.text would output the response from the API
-response_weather = requests.get("http://api.weatherstack.com/current?access_key=2d90d10eae259b217aee8119adc6ae70&query={city}")
+response_weather = requests.get(f"http://api.weatherstack.com/current?access_key=2d90d10eae259b217aee8119adc6ae70&query={city}")
 
 # Parse the JSON data
 current_weather_data_raw = json.loads(response_weather.text)
 
-city_name = data['location']['name']
-country = data['location']['country']
-local_time = data['location']['localtime']
-temperature = data['current']['temperature']
-weather_description = data['current']['weather_descriptions'][0]
+city_name = current_weather_data_raw['location']['name']
+country = current_weather_data_raw['location']['country']
+local_time = current_weather_data_raw['location']['localtime']
+temperature = current_weather_data_raw['current']['temperature']
+weather_description = current_weather_data_raw['current']['weather_descriptions'][0]
 
