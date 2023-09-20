c79a1569b322dc8049a758d713b02b8d89adf718
Pip-1 <philippog@gmail.com>
2023-09-20 21:47:59 +0200

    adding credentials

---
+++ api_creds.py
@@ -0,0 +1,4 @@
+# Access credentials for https://weatherstack.com/documentation API
+
+email = "philippog.werner@gmail.com"
+access_key = "2d90d10eae259b217aee8119adc6ae70"
--- daily_weather_etl.py
+++ daily_weather_etl.py
@@ -2,23 +2,29 @@ import pandas as pd
 from datetime import datetime
 import json
 import requests
-
-access_key = "2d90d10eae259b217aee8119adc6ae70"
+import access_key from api_creds
 
 #creating variable to pass todays date to the API
-today = datetime.today()
+timestamp = datetime.now()
 city = "Erfurt"
 
 #creating variable for the response from the API
 #response.text would output the response from the API
-response_weather = requests.get(f"http://api.weatherstack.com/current?access_key=2d90d10eae259b217aee8119adc6ae70&query={city}")
+response_weather = requests.get(f"http://api.weatherstack.com/current?access_key={access_key}&query={city}")
 
 # Parse the JSON data
 current_weather_data_raw = json.loads(response_weather.text)
 
-city_name = current_weather_data_raw['location']['name']
-country = current_weather_data_raw['location']['country']
-local_time = current_weather_data_raw['location']['localtime']
-temperature = current_weather_data_raw['current']['temperature']
-weather_description = current_weather_data_raw['current']['weather_descriptions'][0]
+# Creating a dictionary to prepare the data for the dataframe
+data_dict = {
+    'timestamp_etl': [timestamp],
+    'local_time': [current_weather_data_raw['location']['localtime']],
+    'city': [current_weather_data_raw['location']['name']],
+    'country': [current_weather_data_raw['location']['country']],
+    'temp_in_c': [current_weather_data_raw['current']['temperature']],
+    'weather_desc': [current_weather_data_raw['current']['weather_descriptions'][0]],
+}
+
+# Creating a dataframe
+weather_df = pd.DataFrame(data_dict)
 
